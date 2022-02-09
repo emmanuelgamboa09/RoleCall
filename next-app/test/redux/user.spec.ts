3d0d1af0cd662a02c58c2f7b75192a1f319e8243
emmanuel_gamboa <emmanuelgamboa09@gmail.com>
2022-02-08 20:48:19 -0800

    Change of string in unit test

--- next-app/test/redux/user.spec.ts
+++ next-app/test/redux/user.spec.ts
@@ -1,6 +1,6 @@
 import userSlice, { updateUser } from "../../redux/slice/userslice";
 
-test("should return the initial empty state", () => {
+test("should return the initial empty user state", () => {
   expect(
     userSlice.reducer(undefined, {
       type: undefined,
@@ -8,7 +8,7 @@ test("should return the initial empty state", () => {
   ).toEqual({ user: {} });
 });
 
-test("should handle a todo being added to an empty list", () => {
+test("should handle user being updated in redux", () => {
   const updatedValues = { email: "test_email@email.com", name: "test name" };
   expect(userSlice.reducer(undefined, updateUser(updatedValues))).toEqual({
     user: updatedValues,

1ecd7e0d3ab246fcc5611f3e809178a43a16d8e8
Sebastian Gwozdz <sebastian.gwozdz@sjsu.edu>
2022-02-11 13:27:55 -0800

    wrapped components with auth0 sdk and added test checking credentials

--- next-app/pages/_app.tsx
+++ next-app/pages/_app.tsx
@@ -1,13 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { UserProvider } from "@auth0/nextjs-auth0";
 import { ThemeProvider } from "@emotion/react";
 import theme from "../src/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <UserProvider>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </UserProvider>
   );
 }
 

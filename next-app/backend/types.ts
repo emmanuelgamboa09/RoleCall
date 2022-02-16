350fd691b621eb2039f563b971ea9a5069d6f147
Mat Seto <seanseto@gmail.com>
2022-02-16 13:50:04 -0800

    Remove unused backend types

--- next-app/backend/types.ts
+++ next-app/backend/types.ts
@@ -1,17 +1,5 @@
-import mongoose from "mongoose";
-
-export interface Substitution {
-  variable: string;
-  val: string;
-}
-
 export interface User {
   _id?: string;
   authId: string;
   name: string;
-}
-
-// HTTP Req Body
-export interface RequestBody {
-  [key: string]: any;
 }

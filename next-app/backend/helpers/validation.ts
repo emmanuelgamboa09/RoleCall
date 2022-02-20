44b014d4d5ff738b105c8066279c2a34d4550d0e
Sebastian Gwozdz <sebastian.gwozdz@sjsu.edu>
2022-02-20 13:32:09 -0800

    name no longer required for user PUT/POST

--- next-app/backend/helpers/validation.ts
+++ next-app/backend/helpers/validation.ts
@@ -1,13 +1,13 @@
 import Joi from "joi";
 
 export const userPOSTSchema = Joi.object({
-  name: Joi.string()
-    .pattern(new RegExp("^[a-zA-Z0-9]{2,30}(\\s[a-zA-Z0-9]{2,30})?$"))
-    .required(),
+  name: Joi.string().pattern(
+    new RegExp("^[a-zA-Z0-9]{2,30}(\\s[a-zA-Z0-9]{2,30})?$")
+  ),
 });
 
 export const userPUTSchema = Joi.object({
-  name: Joi.string()
-    .pattern(new RegExp("^[a-zA-Z0-9]{2,30}(\\s[a-zA-Z0-9]{2,30})?$"))
-    .required(),
+  name: Joi.string().pattern(
+    new RegExp("^[a-zA-Z0-9]{2,30}(\\s[a-zA-Z0-9]{2,30})?$")
+  ),
 });

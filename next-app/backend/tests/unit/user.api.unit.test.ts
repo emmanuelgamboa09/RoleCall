7a14a39fbde8de352bbfd3dd294814dce1d56e6f
Sebastian Gwozdz <sebastian.gwozdz@sjsu.edu>
2022-02-20 14:42:50 -0800

    added endpoint for fetching current user

--- next-app/backend/tests/unit/user.api.unit.test.ts
+++ next-app/backend/tests/unit/user.api.unit.test.ts
@@ -2,7 +2,7 @@ import { expect, test } from "@jest/globals";
 import { NextApiRequest, NextApiResponse } from "next";
 import { createMocks, createRequest, createResponse } from "node-mocks-http";
 import { dbDisconnect } from "../../api/database/dbConnect";
-import { createUser, updateUser } from "../../api/user";
+import { createUser, getUser, updateUser } from "../../api/user";
 import {
   AUTH0_TEST_ID,
   AUTH0_TEST_USER_NAME,
@@ -64,19 +64,6 @@ test("Insert user with empty name", async () => {
   expect(res._getStatusCode()).toBe(400);
 });
 
-test("Insert user with missing name", async () => {
-  const body = {};
-
-  const { req, res } = createMocks({
-    method: "POST",
-    body,
-  });
-
-  await createUser(req, res, AUTH0_TEST_ID, () => Promise.resolve());
-
-  expect(res._getStatusCode()).toBe(400);
-});
-
 test("Insert user but save operation fails", async () => {
   const body = {
     name: AUTH0_TEST_USER_NAME,
@@ -108,8 +95,6 @@ test("Validate incorrect User POST Input", () => {
   const inputs = [
     { name: "" },
     { name: 123 },
-    { name: undefined },
-    {},
     { name: "abc", extraKey: "test" },
     { diffKey: "abc" },
   ];
@@ -173,19 +158,6 @@ test("Update user with empty name", async () => {
   expect(res._getStatusCode()).toBe(400);
 });
 
-test("Update user with empty body", async () => {
-  const body = {};
-
-  const { req, res } = createMocks({
-    method: "PUT",
-    body,
-  });
-
-  await updateUser(req, res, AUTH0_TEST_ID, () => Promise.resolve({}));
-
-  expect(res._getStatusCode()).toBe(400);
-});
-
 test("Update user but save operation fails", async () => {
   const body = {
     name: AUTH0_TEST_USER_NAME,
@@ -232,8 +204,6 @@ test("Validate incorrect User PUT Input", () => {
   const inputs = [
     { _id: "123" },
     { name: 123 },
-    { name: undefined },
-    {},
     { name: "abc", extraKey: "test" },
     { diffKey: "abc" },
     { authId: "123", name: AUTH0_UPDATED_TEST_USER_NAME },
@@ -243,4 +213,53 @@ test("Validate incorrect User PUT Input", () => {
     const { error } = userPUTSchema.validate(val);
     expect(error).toBeTruthy();
   });
+});
+
+test("Get user while authenticated and retrieve operation successful", async () => {
+  const { req, res } = createMocks({
+    method: "GET",
+  });
+
+  await getUser(req, res, AUTH0_TEST_ID, () =>
+    Promise.resolve({ authId: AUTH0_TEST_ID, name: AUTH0_TEST_USER_NAME })
+  );
+
+  expect(res._getStatusCode()).toBe(200);
+  const { authId, name } = JSON.parse(res._getData());
+  expect({ authId, name }).toEqual({
+    authId: AUTH0_TEST_ID,
+    name: AUTH0_TEST_USER_NAME,
+  });
+});
+
+test("Get user while not authenticated", async () => {
+  const { req, res } = createMocks({
+    method: "GET",
+  });
+
+  await getUser(req, res, undefined, () =>
+    Promise.resolve({ authId: AUTH0_TEST_ID })
+  );
+
+  expect(res._getStatusCode()).toBe(401);
+});
+
+test("Get user but retrieve operation fails", async () => {
+  const { req, res } = createMocks({
+    method: "GET",
+  });
+
+  await getUser(req, res, AUTH0_TEST_ID, () => Promise.reject());
+
+  expect(res._getStatusCode()).toBe(500);
+});
+
+test("Get user but user document doesn't exist", async () => {
+  const { req, res } = createMocks({
+    method: "GET",
+  });
+
+  await getUser(req, res, AUTH0_TEST_ID, () => Promise.resolve(null));
+
+  expect(res._getStatusCode()).toBe(404);
 });

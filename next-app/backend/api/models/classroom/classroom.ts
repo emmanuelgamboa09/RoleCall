31710185a25814e3cbe40fd5ca16a94c6a2e7aac
emmanuel_gamboa <emmanuelgamboa09@gmail.com>
2022-02-21 21:44:31 -0800

    intial implementation of classroom model

---
+++ next-app/backend/api/models/classroom/classroom.ts
@@ -0,0 +1,12 @@
+import mongoose, { model, Schema } from "mongoose";
+import { Classroom } from "../../../../interfaces/classroom.interface";
+
+const schema = new Schema<Classroom>({
+  instructorId: { type: String, required: true, index: true },
+  title: { type: String, required: true },
+  students: { type: [String], default: [], required: true, index: true },
+  endDate: { type: Date, required: true },
+});
+
+export const ClassroomModel =
+  mongoose.models.Classroom || model<Classroom>("Classroom", schema);

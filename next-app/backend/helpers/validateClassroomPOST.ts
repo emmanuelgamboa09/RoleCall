a89401ee82180e18625d66f2af49de6f454995a7
Sebastian Gwozdz <38997870+sebastianmgwozdz@users.noreply.github.com>
2022-02-22 19:38:27 -0800

    added endpoint to create classroom document (#30)
    * added endpoint to create classroom document
    
    * removed unused constants
    
    * removed redundant auth check
    
    * Use Classroom TypeScript type annotation for body (#31)
    
    * Use Classroom TypeScript type annotation for body
    
    * Allow HTTPBody type in validateClassroomPOST
    
    Co-authored-by: Sebastian Gwozdz <sebastian.gwozdz@sjsu.edu>
    Co-authored-by: Matthew Seto <46092255+DOORM4T@users.noreply.github.com>

---
+++ next-app/backend/helpers/validateClassroomPOST.ts
@@ -0,0 +1,16 @@
+import { HTTPBody } from './../types';
+import Joi from "joi";
+import { Classroom } from "../../interfaces/classroom.interface";
+
+export default (body: Classroom | HTTPBody) => {
+  const schema = {
+    title: Joi.string()
+      .min(2)
+      .max(16)
+      .pattern(new RegExp("^[a-zA-Z0-9]+(\\s[a-zA-Z0-9]*)?$"))
+      .required(),
+    // Specified end date cannot be in the past
+    endDate: Joi.date().required().greater(Date.now()),
+  };
+  return Joi.object(schema).validate(body);
+};
---
+++ next-app/pages/api/classrooms/index.ts
@@ -0,0 +1,36 @@
+import { withApiAuthRequired } from "@auth0/nextjs-auth0";
+import { HydratedDocument } from "mongoose";
+import { NextApiRequest, NextApiResponse } from "next";
+import { createClassroom } from "../../../backend/api/classroom";
+import { ClassroomModel } from "../../../backend/api/models/classroom";
+import { getAuthId } from "../../../backend/helpers/getAuthId";
+import { Classroom } from "../../../interfaces/classroom.interface";
+
+export default withApiAuthRequired(async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  const { method } = request;
+
+  switch (method) {
+    case "POST": {
+      await createClassroom(
+        request,
+        response,
+        getAuthId(request, response)!,
+        async (classroom: Classroom) => {
+          const doc: HydratedDocument<Classroom> = new ClassroomModel(
+            classroom
+          );
+          await doc.save();
+        }
+      );
+      break;
+    }
+    case "GET": {
+      response.status(200).send("Successful Classrooms GET");
+      break;
+    }
+    default:
+  }
+});

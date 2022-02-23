becd15999569b57e6fd5e18b17cea544518ecd58
emmanuelgamboa09 <61162551+emmanuelgamboa09@users.noreply.github.com>
2022-02-22 19:28:28 -0800

    Feature/random key generator (#33)
    * Key Generator For Classrooms

---
+++ next-app/backend/tests/unit/keyGenerator.helper.unit.test.ts
@@ -0,0 +1,18 @@
+import { expect, test } from "@jest/globals";
+import KeyGenerator, { generatorOptions } from "../../helpers/keyGenerator";
+
+test("Generate random string with generator options", () => {
+  KeyGenerator.get((error: any, code: any) => {
+    expect(error).toBeNull;
+
+    const generatedCodeWithoutSplit = code.replaceAll("-", "");
+    expect(generatedCodeWithoutSplit.length).toEqual(generatorOptions.length);
+
+    if (generatorOptions.splitStatus === true) {
+      const splitCode = code.split(generatorOptions.split);
+      expect(splitCode.length).toEqual(
+        Math.ceil(generatorOptions.length / generatorOptions.group)
+      );
+    }
+  });
+});

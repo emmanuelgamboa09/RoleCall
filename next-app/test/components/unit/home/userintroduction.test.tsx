bcc7fb45da205d1098ae33878a4f0e62196034e1
emmanuel_gamboa <emmanuelgamboa09@gmail.com>
2022-02-23 19:15:25 -0800

    Fix mock test and added jest-fetch-mock to dev dependency

--- next-app/test/components/unit/home/userintroduction.test.tsx
+++ next-app/test/components/unit/home/userintroduction.test.tsx
@@ -7,10 +7,19 @@ import configureMockStore from "redux-mock-store";
 import { Provider } from "react-redux";
 import TestRenderer from "react-test-renderer";
 import UserIntroduction from "../../../../components/home_page/userintroduction";
+import { UserProvider } from "@auth0/nextjs-auth0";
+import { enableFetchMocks } from "jest-fetch-mock";
+import fetchMock from "jest-fetch-mock";
+enableFetchMocks();
 
 const mockStore = configureMockStore([]);
+afterEach(() => {
+  fetchMock.resetMocks();
+});
 
 test("Displays users name in userintroduction component", () => {
+  fetchMock.mockResponse(JSON.stringify({ name: "test name" }));
+
   const store = mockStore({
     userReducer: {
       user: { name: "test name" },
@@ -18,9 +27,11 @@ test("Displays users name in userintroduction component", () => {
   });
 
   const render = TestRenderer.create(
-    <Provider store={store}>
-      <UserIntroduction />
-    </Provider>
+    <UserProvider>
+      <Provider store={store}>
+        <UserIntroduction />
+      </Provider>
+    </UserProvider>
   );
 
   const testInstance = render.root;
@@ -31,22 +42,31 @@ test("Displays users name in userintroduction component", () => {
   ]);
 });
 
-test("DOesn't display users name in userintroduction component", () => {
+test("Doesn't display users name in userintroduction component", async () => {
+  fetchMock.mockResponse(JSON.stringify({}));
+
   const store = mockStore({
     userReducer: {
-      user: null,
+      user: {},
     },
   });
 
-  const render = TestRenderer.create(
-    <Provider store={store}>
-      <UserIntroduction />
-    </Provider>
-  );
+  let render: TestRenderer.ReactTestRenderer | null = null;
+  await TestRenderer.act(async () => {
+    render = TestRenderer.create(
+      <UserProvider>
+        <Provider store={store}>
+          <UserIntroduction />
+        </Provider>
+      </UserProvider>
+    );
+  });
 
-  const testInstance = render.root;
-  expect(testInstance.findByProps({ id: "userName" }).props.children).toEqual([
-    "Hello ",
-    null,
-  ]);
+  if (render) {
+    const test = render as TestRenderer.ReactTestRenderer;
+    const testInstance = test.root;
+    expect(testInstance.findByProps({ id: "userName" }).props.children).toEqual(
+      ["Hello ", undefined]
+    );
+  }
 });

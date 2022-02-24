6008ce89ffe29bee6a979b7c219d5ef25e103a3b
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2022-02-23 17:29:30 -0800

    Test auth-based redirects with Cypress (#37)
    * Redirect authenticated user from Landing page to app
    
    * Protect Home Page with Auth0
    
    * Set up Cypress for E2E tests
    
    * Set up E2E Auth0 login tests in Cypress
    
    * Test navigation between landing and app pages

---
+++ next-app/cypress/integration/landing.spec.ts
@@ -0,0 +1,34 @@
+import Pages from '../../constants/Pages.enum'
+
+const landingPagePath = Pages.Landing
+const appPagePath = Pages.App
+
+describe("Navigation Between Landing & App Pages", () => {
+    context("Not logged in", () => {
+        before(() => { cy.clearAuth0Cookies() })
+
+        it("should load landing page", () => {
+            cy.visit(landingPagePath)
+            cy.location("pathname").should('equal', landingPagePath)
+        })
+
+        it("should redirect from app page to Auth0 sign in", () => {
+            cy.visit(appPagePath)
+            cy.location("pathname").should('not.equal', appPagePath)
+        })
+    })
+    context("Logged in", () => {
+        before(() => { cy.clearAuth0Cookies().login() })
+        after(() => { cy.clearAuth0Cookies() })
+
+        it("should load app page", () => {
+            cy.visit(appPagePath)
+            cy.location("pathname").should('equal', appPagePath)
+        })
+
+        it("should redirect from landing page to app page", () => {
+            cy.visit(landingPagePath)
+            cy.location("pathname").should('equal', appPagePath)
+        })
+    })
+})

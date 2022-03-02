6bb6a50e6ba55ed343cc57b83c7f10ec0daec658
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2022-03-01 19:32:34 -0800

    Set up ClassroomTabs

---
+++ next-app/components/classroom/ClassroomTabs.tsx
@@ -0,0 +1,26 @@
+import { Box, Tab, Tabs } from "@mui/material"
+import { useState } from "react"
+
+export type ClassroomTabs = { [name: string]: { content: JSX.Element } }
+
+export interface ClassroomTabsProps {
+    tabs: ClassroomTabs
+    initialTab?: string
+}
+
+const ClassroomTabs = ({ tabs, initialTab }: ClassroomTabsProps) => {
+    const [tab, setTab] = useState<string>(initialTab ?? Object.keys(tabs)[0])
+
+    return <>
+        <Tabs value={tab} onChange={(_, newValue) => setTab(newValue)} aria-label="classroom tabs" centered>
+            {
+                Object.keys(tabs).map((name) => {
+                    return <Tab label={name} value={name} />
+                })
+            }
+        </Tabs>
+        {tab in tabs && tabs[tab].content}
+    </>
+}
+
+export default ClassroomTabs
--- next-app/pages/app/classroom/[classroom].tsx
+++ next-app/pages/app/classroom/[classroom].tsx
@@ -1,23 +1,18 @@
 
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
-import { Box, Tab, Tabs, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
-import { ReactElement, useState } from "react";
+import { ReactElement } from "react";
+import ClassroomTabs from "../../../components/classroom/ClassroomTabs";
 import BaseAppLayout from "../../../layout/baseapplayout";
 import theme from "../../../src/theme";
 
-enum ClassroomTab {
-    PROJECT = "project",
-    TEAM_FINDER = "team-finder",
-    MY_TEAM = "my-team"
-}
 
 const ClassroomPage: NextPageWithLayout = () => {
     const router = useRouter()
     const { classroom } = router.query as { classroom: string }
 
-    const [tab, setTab] = useState<ClassroomTab>(ClassroomTab.PROJECT)
 
     return (
         <Box
@@ -32,17 +27,13 @@ const ClassroomPage: NextPageWithLayout = () => {
             <Typography component="h2" variant="h4">
                 Instructor: Foo Bar
             </Typography>
-            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                <Tabs value={tab} onChange={(_, newValue) => setTab(newValue)} aria-label="classroom tabs">
-                    <Tab label="Item One" value={ClassroomTab.PROJECT} />
-                    <Tab label="Item Two" value={ClassroomTab.TEAM_FINDER} />
-                    <Tab label="Item Three" value={ClassroomTab.MY_TEAM} />
-                </Tabs>
-                {tab === ClassroomTab.PROJECT && <div>Project</div>}
-                {tab === ClassroomTab.TEAM_FINDER && <div>Team Finder</div>}
-                {tab === ClassroomTab.MY_TEAM && <div>My Team</div>}
-            </Box >
-
+            <ClassroomTabs
+                tabs={{
+                    "Project": { content: <div>Project</div> },
+                    "Team Finder": { content: <div>Team Finder</div> },
+                    "My Team": { content: <div>My Team</div> },
+                }}
+            />
         </Box >
     );
 }

2c0fe05be12bf7b4e932a475f4c24da888c2ad8f
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2022-04-07 11:26:11 -0700

    Integration tests for getProjectUser

--- next-app/backend/api/project-user/getProjectUser.ts
+++ next-app/backend/api/project-user/getProjectUser.ts
@@ -6,18 +6,26 @@ import { UserProjectProfile } from "../../database/models/project/userProjectPro
 import { FindById } from "../../types";
 import { UserModel } from "./../../database/models/user";
 
-export type Data =
-  | Omit<
-      UserProjectProfile,
-      "_id" | "studentId" | "incomingTeamRequests" | "outgoingTeamRequests"
-    >
-  | {
-      message:
-        | "not-created"
-        | "server-error"
-        | "invalid-request"
-        | "not-authorized";
-    };
+export type Data = ProfileData | Message;
+
+export type ProfileData = Omit<
+  UserProjectProfile,
+  "_id" | "studentId" | "incomingTeamRequests" | "outgoingTeamRequests"
+>;
+
+export type Message = {
+  message:
+    | "not-created"
+    | "server-error"
+    | "invalid-request"
+    | "not-authorized";
+  reason?: string;
+};
+
+export type Query = {
+  profileId?: string;
+  projectId?: string;
+};
 
 export default async (
   req: NextApiRequest,
@@ -27,10 +35,7 @@ export default async (
 ) => {
   try {
     const { query } = req;
-    const { profileId, projectId } = query as {
-      profileId?: string;
-      projectId?: string;
-    };
+    const { profileId, projectId } = query as Query;
 
     if (!profileId) throw new Error("profileId not specified");
     if (!projectId) throw new Error("projectId not specified");
@@ -58,7 +63,7 @@ export default async (
     console.log(isViewerInstructor, isViewerProjectUser);
 
     if (!isViewerInstructor && !isViewerProjectUser) {
-      return res.json({ message: "not-authorized" });
+      return res.status(400).json({ message: "not-authorized" });
     }
 
     const { desiredRoles, projectBio } = profile;
@@ -66,8 +71,13 @@ export default async (
     return res.status(200).json({ desiredRoles, projectBio });
   } catch (e) {
     if (e instanceof MongooseError) {
-      res.status(500).json({ message: "server-error" });
+      res.status(500).json({
+        message: "server-error",
+        reason: (e as MongooseError).message,
+      });
     }
-    return res.status(400).json({ message: "invalid-request" });
+    return res
+      .status(400)
+      .json({ message: "invalid-request", reason: (e as Error).message });
   }
 };

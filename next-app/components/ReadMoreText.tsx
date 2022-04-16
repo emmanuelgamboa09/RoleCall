b8cf757eb8b3c3fc8f63c8bf378de16cbbcdd87d
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2022-04-15 17:32:28 -0700

    Create ReadMoreText component

---
+++ next-app/components/ReadMoreText.tsx
@@ -0,0 +1,63 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogProps,
+  DialogTitle,
+  Link as MuiLink,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+
+export type ReadMoreTextProps = {
+  title: string;
+  content: string;
+  cutoffLength: number;
+  scroll?: DialogProps["scroll"];
+};
+
+const ReadMoreText = ({
+  title,
+  content,
+  cutoffLength,
+  scroll = "paper",
+}: ReadMoreTextProps) => {
+  const [readMore, setReadMore] = useState(false);
+
+  const handleOpen = () => {
+    setReadMore(true);
+  };
+
+  const handleClose = () => {
+    setReadMore(false);
+  };
+
+  return (
+    <Typography component="body" fontSize="16px">
+      {content.length > cutoffLength ? (
+        <>
+          {content.slice(0, cutoffLength)}...
+          <br />
+          <MuiLink href="#" onClick={handleOpen}>
+            Read More
+          </MuiLink>
+        </>
+      ) : (
+        content
+      )}
+      <Dialog open={readMore} onClose={handleClose} scroll={scroll}>
+        <DialogTitle>{title}</DialogTitle>
+        <DialogContent dividers>
+          <DialogContentText>{content}</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Close</Button>
+        </DialogActions>
+      </Dialog>
+    </Typography>
+  );
+};
+
+export default ReadMoreText;
